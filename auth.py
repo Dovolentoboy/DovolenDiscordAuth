0c002cfa62662e4c7ca880ae1177fc1ac306c072
Nikita <dimavlaskin9@gmail.com>
2023-10-11 23:08:12 +0300

    рефактор auth.py. Внесение значительных изменений

--- auth.py
+++ auth.py
@@ -1,5 +1,14 @@
 import requests
+import string
+import random
 
+
+class Auth:
+    @staticmethod
+    def generate_random_password(lenght=10):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(lenght))
+        return password
 
 class AuthDb:
     def __init__(self):
@@ -9,26 +18,35 @@ class AuthDb:
         self.user = None
         self.permission = None
 
-    def set_settings(self, discord_model, user_data, guilds_data, user, permission):
+    def set_settings(self, discord_model = None, user_data = None, guilds_data = None, user = None, permission = None, user_modal = None):
         self.discord_model = discord_model
         self.user_data = user_data
         self.guilds_data = guilds_data
         self.user = user
         self.permission = permission
 
-    def discord_model_operations(self) -> None:
+    def user_model_create(self):
+        try:
+            user = self.user_model.objects.get(username=self.user_data["username"])
+            user.username = self.user.username
+            user.save()
+        except self.user_model.DoesNotExist:
+            user = self.user_model.objects.create_user(username=self.user_data["username"], password=Auth.generate_random_password())
+        return user
+
+    def discord_model_create(self) -> None:
         for user_id in self.user_data:
             id = user_id['id'] 
         try:
             obj = self.discord_model.objects.get(user=self.user)
             obj.user_data = self.user_data
             obj.discord_user_id = id
-            obj.guilds_data = self.check_permissions(self.guilds_data, self.permission)
+            obj.guilds_data = self.guilds_data
             obj.save()
         except self.discord_model.DoesNotExist:
             obj = self.discord_model.objects.create(
                 user=self.user,
                 user_data=self.user_data,
-                guilds_data=self.discord_auth.check_permissions(self.guilds_data),
+                guilds_data=self.guilds_data,
                 discord_user_id=id
             )
